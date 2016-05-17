482cb98bdcd29e6f3cfef3f4c1b29e397eb451b9
GarretRCorbett <GarretRCorbett@gmail.com>
2016-05-17 19:44:00 -0400

    adding an index file

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Github HTML Project</title>
+    </head>
+    
+    
+    
+    <body>
+        <header>
+        
+        
+        </header>
+        <nav>
+        
+        
+        </nav>
+        <section>
+        
+        
+        
+        </section>
+        <footer>
+            <p>&copy;CWP <?php echo date('Y'); ?></p>
+        </footer>
+    </body>
+    
+</html>
